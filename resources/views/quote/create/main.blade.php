c3a0a4ae0f838df21131863734d79c6f6c9033d8
develop <develop@test0092.axcell28.idv.tw>
2022-12-06 14:47:16 +0800

    layout: 報價新增主頁 內容填充與設計

--- resources/views/quote/create/main.blade.php
+++ resources/views/quote/create/main.blade.php
@@ -17,7 +17,63 @@
             </div>
             <form method='post' action='/quote/create/main' class='form1' enctype="multipart/form-data">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                <p class=""> <button class="btn">新增</button> </p>
+                <div class="show-line2">
+                    <h5>報價類別</h5>
+                    <p>
+                        <select name="quoteCls">
+                            <option value="1">業務一部</option>
+                            <option value="2">業務二部</option>
+                            <option value="3">公司品項</option>
+                        </select>
+                    </p>
+                </div>
+                <div class="show-line2">
+                    <h5>客人產品編號</h5>
+                    <p> <input type="text" name="customerProductNum" required /> </p>
+                </div>
+                <div class="show-line2">
+                    <h5>產品編號</h5>
+                    <p> <input type="text" name="productNum" required /> </p>
+                </div>
+                <div class="show-line2">
+                    <h5>中文產品名稱</h5>
+                    <p> <input type="text" name="productNameTw" required /> </p>
+                </div>
+                <div class="show-line2">
+                    <h5>英文產品名稱</h5>
+                    <p> <input type="text" name="productNameEn" /> </p>
+                </div>
+                <div class="show-line2">
+                    <h5>品質要求</h5>
+                    <p>
+                        <select name="quoteCls">
+                            <option value="高">高</option>
+                            <option value="中高">中高</option>
+                            <option value="普通">普通</option>
+                            <option value="低">低</option>
+                        </select>
+                    </p>
+                </div>
+                <div class="show-line2">
+                    <h5>報價數量</h5>
+                    <p>
+                        <select name="quoteQuantity">
+                            <option value="MOQ-1K">MOQ(1K)</option>
+                            <option value="3K">3K</option>
+                            <option value="5K">5K</option>
+                            <option value="10K">10K</option>
+                        </select>
+                    </p>
+                </div>
+                <div class="show-line2">
+                    <h5>插入圖片</h5>
+                    <p> <input type="file" name="image" /> </p>
+                </div>
+                <div class="show-line3">
+                    <h5>產品說明</h5>
+                    <p> <textarea name="productInfo" ></textarea> </p>
+                </div>
+                <p class=""> <button class="btn">建立</button> </p>
             </form>
         </div>
 @include('member.layout.footer')
