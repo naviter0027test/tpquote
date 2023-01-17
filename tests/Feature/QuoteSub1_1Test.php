6308e78b9ace70f321bc5ec2f0d52f66fabd982b
Lanker <dwu446227@gmail.com>
2023-01-17 11:47:26 +0800

    tdd: Unit quote sub1-1 雛型

---
+++ tests/Feature/QuoteSub1_1Test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Repositories\QuoteRepository;
+use Tests\TestCase;
+
+class QuoteSub1_1Test extends TestCase
+{
+    public function setUp() : void {
+        parent::setUp();
+        shell_exec('php artisan migrate --path=/database/migrations/20221207/');
+        shell_exec('php artisan db:seed --class=QuoteMainSeeder');
+        shell_exec('php artisan db:seed --class=QuoteSub1Seeder');
+    }
+
+    public function tearDown() : void {
+        shell_exec('php artisan DropTables');
+    }
+
+    public function testGetSub1_1ByMainId() {
+        $this->assertEquals(true, true);
+    }
+}
