78e8864d4c9622f4483633e6773de1f5bbcfa737
Lanker <dwu446227@gmail.com>
2023-02-15 16:44:38 +0800

    seed: QuoteSub3 +10 items

--- database/seeders/QuoteSub3Seeder.php
+++ database/seeders/QuoteSub3Seeder.php
@@ -79,5 +79,135 @@ class QuoteSub3Seeder extends Seeder
         $quoteSub3at5->info = "";
         $quoteSub3at5->infoImg = "";
         $quoteSub3at5->save();
+
+        $quoteSub3at6 = new QuoteSub3();
+        $quoteSub3at6->mainId = 6;
+        $quoteSub3at6->partNo = "SUB1-20221200006";
+        $quoteSub3at6->materialName = "磁石";
+        $quoteSub3at6->length = 70;
+        $quoteSub3at6->width = 43;
+        $quoteSub3at6->height = 29;
+        $quoteSub3at6->usageAmount = 62;
+        $quoteSub3at6->spec = "釘帽直徑6mm";
+        $quoteSub3at6->info = "";
+        $quoteSub3at6->infoImg = "";
+        $quoteSub3at6->save();
+
+        $quoteSub3at7 = new QuoteSub3();
+        $quoteSub3at7->mainId = 7;
+        $quoteSub3at7->partNo = "SUB1-20221200007";
+        $quoteSub3at7->materialName = "PVC片";
+        $quoteSub3at7->length = 38;
+        $quoteSub3at7->width = 59;
+        $quoteSub3at7->height = 90;
+        $quoteSub3at7->usageAmount = 28;
+        $quoteSub3at7->spec = "孔徑2mm";
+        $quoteSub3at7->info = "";
+        $quoteSub3at7->infoImg = "";
+        $quoteSub3at7->save();
+
+        $quoteSub3at8 = new QuoteSub3();
+        $quoteSub3at8->mainId = 8;
+        $quoteSub3at8->partNo = "SUB1-20221200008";
+        $quoteSub3at8->materialName = "PVC盒";
+        $quoteSub3at8->length = 94;
+        $quoteSub3at8->width = 73;
+        $quoteSub3at8->height = 44;
+        $quoteSub3at8->usageAmount = 79;
+        $quoteSub3at8->spec = "釘帽直徑6mm";
+        $quoteSub3at8->info = "";
+        $quoteSub3at8->infoImg = "";
+        $quoteSub3at8->save();
+
+        $quoteSub3at9 = new QuoteSub3();
+        $quoteSub3at9->mainId = 9;
+        $quoteSub3at9->partNo = "SUB1-20221200009";
+        $quoteSub3at9->materialName = "PVC板";
+        $quoteSub3at9->length = 98;
+        $quoteSub3at9->width = 79;
+        $quoteSub3at9->height = 46;
+        $quoteSub3at9->usageAmount = 35;
+        $quoteSub3at9->spec = "釘帽直徑6mm";
+        $quoteSub3at9->info = "";
+        $quoteSub3at9->infoImg = "";
+        $quoteSub3at9->save();
+
+        $quoteSub3at10 = new QuoteSub3();
+        $quoteSub3at10->mainId = 10;
+        $quoteSub3at10->partNo = "SUB1-20221200010";
+        $quoteSub3at10->materialName = "PVC吸塑";
+        $quoteSub3at10->length = 120;
+        $quoteSub3at10->width = 80;
+        $quoteSub3at10->height = 40;
+        $quoteSub3at10->usageAmount = 50;
+        $quoteSub3at10->spec = "H9mm";
+        $quoteSub3at10->info = "";
+        $quoteSub3at10->infoImg = "";
+        $quoteSub3at10->save();
+
+        $quoteSub3at11 = new QuoteSub3();
+        $quoteSub3at11->mainId = 11;
+        $quoteSub3at11->partNo = "SUB1-20221200011";
+        $quoteSub3at11->materialName = "PET片";
+        $quoteSub3at11->length = 125;
+        $quoteSub3at11->width = 40;
+        $quoteSub3at11->height = 70;
+        $quoteSub3at11->usageAmount = 60;
+        $quoteSub3at11->spec = "孔深8mm";
+        $quoteSub3at11->info = "";
+        $quoteSub3at11->infoImg = "";
+        $quoteSub3at11->save();
+
+        $quoteSub3at12 = new QuoteSub3();
+        $quoteSub3at12->mainId = 12;
+        $quoteSub3at12->partNo = "SUB1-20221200012";
+        $quoteSub3at12->materialName = "PET袋";
+        $quoteSub3at12->length = 135;
+        $quoteSub3at12->width = 38;
+        $quoteSub3at12->height = 60;
+        $quoteSub3at12->usageAmount = 20;
+        $quoteSub3at12->spec = "H9mm";
+        $quoteSub3at12->info = "";
+        $quoteSub3at12->infoImg = "";
+        $quoteSub3at12->save();
+
+        $quoteSub3at13 = new QuoteSub3();
+        $quoteSub3at13->mainId = 13;
+        $quoteSub3at13->partNo = "SUB1-20221200013";
+        $quoteSub3at13->materialName = "PET盒";
+        $quoteSub3at13->length = 160;
+        $quoteSub3at13->width = 80;
+        $quoteSub3at13->height = 90;
+        $quoteSub3at13->usageAmount = 30;
+        $quoteSub3at13->spec = "釘帽直徑6mm";
+        $quoteSub3at13->info = "";
+        $quoteSub3at13->infoImg = "";
+        $quoteSub3at13->save();
+
+        $quoteSub3at14 = new QuoteSub3();
+        $quoteSub3at14->mainId = 14;
+        $quoteSub3at14->partNo = "SUB1-20221200014";
+        $quoteSub3at14->materialName = "PET罐";
+        $quoteSub3at14->length = 165;
+        $quoteSub3at14->width = 37;
+        $quoteSub3at14->height = 85;
+        $quoteSub3at14->usageAmount = 40;
+        $quoteSub3at14->spec = "孔徑2mm";
+        $quoteSub3at14->info = "";
+        $quoteSub3at14->infoImg = "";
+        $quoteSub3at14->save();
+
+        $quoteSub3at15 = new QuoteSub3();
+        $quoteSub3at15->mainId = 15;
+        $quoteSub3at15->partNo = "SUB1-20221200015";
+        $quoteSub3at15->materialName = "雙面覆膠軟鐵";
+        $quoteSub3at15->length = 189;
+        $quoteSub3at15->width = 230;
+        $quoteSub3at15->height = 15;
+        $quoteSub3at15->usageAmount = 100;
+        $quoteSub3at15->spec = "H9mm";
+        $quoteSub3at15->info = "";
+        $quoteSub3at15->infoImg = "";
+        $quoteSub3at15->save();
     }
 }
