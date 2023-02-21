1baf706450fa56243e14ed47f3c4a88ad1a3a773
Lanker <dwu446227@gmail.com>
2023-02-21 11:44:59 +0800

    tdd: QuoteSub3_1Test getSub3_1ByMainId 測'指定不存在'

--- tests/Feature/QuoteSub3_1Test.php
+++ tests/Feature/QuoteSub3_1Test.php
@@ -33,5 +33,20 @@ class QuoteSub3_1Test extends TestCase
         $this->assertEquals("滾漆", $quoteSub3_1at1->name);
         $this->assertEquals("二底一面", $quoteSub3_1at1->painted);
         $this->assertEquals(1500, $quoteSub3_1at1->subtotal);
+
+        try {
+            $quoteSub3_1at2 = $quoteRepo->getSub3_1ByMainId(99);
+            $this->assertEquals(true, false);
+        }
+        catch(Exception $e) {
+            $this->assertEquals("指定資料不存在", $e->getMessage());
+        }
+
+        $quoteSub3_1at3 = $quoteRepo->getSub3_1ByMainId(17);
+        $this->assertEquals(17, $quoteSub3_1at3->mainId);
+        $this->assertEquals("SLN-20221200017", $quoteSub3_1at3->serialNumber);
+        $this->assertEquals("噴漆", $quoteSub3_1at3->name);
+        $this->assertEquals("二底一面", $quoteSub3_1at3->painted);
+        $this->assertEquals(4550, $quoteSub3_1at3->subtotal);
     }
 }
