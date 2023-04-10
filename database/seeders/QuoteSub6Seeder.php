f5806ea46c681ff84f296082123f45ad8f73e17f
Lanker <naviter0027test@gmail.com>
2023-04-10 17:04:41 +0800

    seed: sub6 +13 items

--- database/seeders/QuoteSub6Seeder.php
+++ database/seeders/QuoteSub6Seeder.php
@@ -24,5 +24,135 @@ class QuoteSub6Seeder extends Seeder
         $quoteSub6at1->localNeedSec = 25;
         $quoteSub6at1->usageAmount = 400;
         $quoteSub6at1->save();
+
+        $quoteSub6at2 = new QuoteSub6();
+        $quoteSub6at2->mainId = 2;
+        $quoteSub6at2->serialNumber = "SLN-20221200002";
+        $quoteSub6at2->processName = "裁面板";
+        $quoteSub6at2->materialName = "展示盒";
+        $quoteSub6at2->processMemo = "";
+        $quoteSub6at2->localNeedSec = 95;
+        $quoteSub6at2->usageAmount = 250;
+        $quoteSub6at2->save();
+
+        $quoteSub6at3 = new QuoteSub6();
+        $quoteSub6at3->mainId = 3;
+        $quoteSub6at3->serialNumber = "SLN-20221200003";
+        $quoteSub6at3->processName = "面板滾漆";
+        $quoteSub6at3->materialName = "盒身";
+        $quoteSub6at3->processMemo = "";
+        $quoteSub6at3->localNeedSec = 120;
+        $quoteSub6at3->usageAmount = 140;
+        $quoteSub6at3->save();
+
+        $quoteSub6at4 = new QuoteSub6();
+        $quoteSub6at4->mainId = 4;
+        $quoteSub6at4->serialNumber = "SLN-20221200004";
+        $quoteSub6at4->processName = "貼紙";
+        $quoteSub6at4->materialName = "盒底";
+        $quoteSub6at4->processMemo = "";
+        $quoteSub6at4->localNeedSec = 180;
+        $quoteSub6at4->usageAmount = 70;
+        $quoteSub6at4->save();
+
+        $quoteSub6at5 = new QuoteSub6();
+        $quoteSub6at5->mainId = 5;
+        $quoteSub6at5->serialNumber = "SLN-20221200005";
+        $quoteSub6at5->processName = "底板貼紙";
+        $quoteSub6at5->materialName = "盒內邊條";
+        $quoteSub6at5->processMemo = "";
+        $quoteSub6at5->localNeedSec = 185;
+        $quoteSub6at5->usageAmount = 80;
+        $quoteSub6at5->save();
+
+        $quoteSub6at6 = new QuoteSub6();
+        $quoteSub6at6->mainId = 6;
+        $quoteSub6at6->serialNumber = "SLN-20221200006";
+        $quoteSub6at6->processName = "面板熱轉印";
+        $quoteSub6at6->materialName = "盒蓋短邊條";
+        $quoteSub6at6->processMemo = "";
+        $quoteSub6at6->localNeedSec = 115;
+        $quoteSub6at6->usageAmount = 280;
+        $quoteSub6at6->save();
+
+        $quoteSub6at7 = new QuoteSub6();
+        $quoteSub6at7->mainId = 7;
+        $quoteSub6at7->serialNumber = "SLN-20221200007";
+        $quoteSub6at7->processName = "鑽孔";
+        $quoteSub6at7->materialName = "膠磁";
+        $quoteSub6at7->processMemo = "";
+        $quoteSub6at7->localNeedSec = 130;
+        $quoteSub6at7->usageAmount = 320;
+        $quoteSub6at7->save();
+
+        $quoteSub6at8 = new QuoteSub6();
+        $quoteSub6at8->mainId = 8;
+        $quoteSub6at8->serialNumber = "SLN-20221200008";
+        $quoteSub6at8->processName = "面板合框+品檢";
+        $quoteSub6at8->materialName = "單面背膠膠磁";
+        $quoteSub6at8->processMemo = "";
+        $quoteSub6at8->localNeedSec = 130;
+        $quoteSub6at8->usageAmount = 320;
+        $quoteSub6at8->save();
+
+        $quoteSub6at9 = new QuoteSub6();
+        $quoteSub6at9->mainId = 9;
+        $quoteSub6at9->serialNumber = "SLN-20221200009";
+        $quoteSub6at9->processName = "沖壓一刀";
+        $quoteSub6at9->materialName = "雙面覆膠軟鐵";
+        $quoteSub6at9->processMemo = "";
+        $quoteSub6at9->localNeedSec = 90;
+        $quoteSub6at9->usageAmount = 80;
+        $quoteSub6at9->save();
+
+        $quoteSub6at10 = new QuoteSub6();
+        $quoteSub6at10->mainId = 10;
+        $quoteSub6at10->serialNumber = "SLN-20221200010";
+        $quoteSub6at10->processName = "沖壓二刀+撥取出品檢";
+        $quoteSub6at10->materialName = "鉚釘";
+        $quoteSub6at10->processMemo = "";
+        $quoteSub6at10->localNeedSec = 95;
+        $quoteSub6at10->usageAmount = 110;
+        $quoteSub6at10->save();
+
+        $quoteSub6at11 = new QuoteSub6();
+        $quoteSub6at11->mainId = 11;
+        $quoteSub6at11->serialNumber = "SLN-20221200011";
+        $quoteSub6at11->processName = "噴漆";
+        $quoteSub6at11->materialName = "強磁";
+        $quoteSub6at11->processMemo = "";
+        $quoteSub6at11->localNeedSec = 210;
+        $quoteSub6at11->usageAmount = 130;
+        $quoteSub6at11->save();
+
+        $quoteSub6at12 = new QuoteSub6();
+        $quoteSub6at12->mainId = 12;
+        $quoteSub6at12->serialNumber = "SLN-20221200012";
+        $quoteSub6at12->processName = "絲印";
+        $quoteSub6at12->materialName = "磁石";
+        $quoteSub6at12->processMemo = "";
+        $quoteSub6at12->localNeedSec = 230;
+        $quoteSub6at12->usageAmount = 150;
+        $quoteSub6at12->save();
+
+        $quoteSub6at13 = new QuoteSub6();
+        $quoteSub6at13->mainId = 13;
+        $quoteSub6at13->serialNumber = "SLN-20221200013";
+        $quoteSub6at13->processName = "修邊";
+        $quoteSub6at13->materialName = "PVC片";
+        $quoteSub6at13->processMemo = "";
+        $quoteSub6at13->localNeedSec = 210;
+        $quoteSub6at13->usageAmount = 160;
+        $quoteSub6at13->save();
+
+        $quoteSub6at14 = new QuoteSub6();
+        $quoteSub6at14->mainId = 14;
+        $quoteSub6at14->serialNumber = "SLN-20221200014";
+        $quoteSub6at14->processName = "底板烙印、";
+        $quoteSub6at14->materialName = "PVC盒";
+        $quoteSub6at14->processMemo = "";
+        $quoteSub6at14->localNeedSec = 220;
+        $quoteSub6at14->usageAmount = 170;
+        $quoteSub6at14->save();
     }
 }
