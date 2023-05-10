bb7599be20bbc24b553e336b9730ed5c52be5188
Lanker <naviter0027test@gmail.com>
2023-05-10 15:39:41 +0800

    tdd: /quote/edit/sub9/{mainId} GET 權限檢查, 指定資料不存在檢查, 讀取

--- routes/web.php
+++ routes/web.php
@@ -115,6 +115,8 @@ Route::middleware([CheckMemberLoginMiddle::class])->group(function() {
         Route::get('edit/sub8/{mainId}', [QuoteController::class, 'editSub8']);
         Route::post('create/sub8/{mainId}', [QuoteController::class, 'createSub8']);
         Route::post('edit/sub8/{mainId}', [QuoteController::class, 'updateSub8']);
+
+        Route::get('edit/sub9/{mainId}', [QuoteController::class, 'editSub9']);
     });
 });
 
